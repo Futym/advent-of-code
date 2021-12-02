9024e57d94abebdab70f30b327ce7087694e45bb
Futym <mfutyma2000@gmail.com>
2021-12-03 00:28:07 +0100

    AoC2021 Day 2v2

--- 2021/Day2/Day2_2.py
+++ 2021/Day2/Day2_2.py
@@ -1,16 +1,18 @@
 file=open("Day2/Day2.txt")
 depth=0
 forward=0
+aim=0
 while True:
     inst=file.readline()
     if(inst):
         inst=inst.split()
         if(inst[0]=="up"):
-            depth-=int(inst[1])
+            aim-=int(inst[1])
         if(inst[0]=="down"):
-            depth+=int(inst[1])
+            aim+=int(inst[1])
         if(inst[0]=="forward"):
             forward+=int(inst[1])
+            depth+=aim*int(inst[1])
     else:
         break
 print(depth*forward)
