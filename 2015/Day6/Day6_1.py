217d1ac28e1427397cc62b3e8ee0dcfccd57247f
Futym <mfutyma2000@gmail.com>
2022-11-30 14:48:07 +0100

    Day 6 year 2015

---
+++ 2015/Day6/Day6_1.py
@@ -0,0 +1,30 @@
+import numpy
+import math
+import os
+import pathlib
+
+def readfile(filename):
+    with open(filename, "r") as file:
+        return file.readlines()
+
+
+if __name__ == "__main__":
+    path = pathlib.Path(__file__).parent.resolve()
+    instructions = readfile(os.path.join(path, "input.txt")) 
+    roof = numpy.zeros((1000, 1000))
+    for instruction in instructions:
+        splitted_instruction = instruction.split(" ")
+        type_of_instruction = splitted_instruction[0]
+        if (type_of_instruction == "turn"):
+            for i in range(int(splitted_instruction[2].split(",")[0]), int(splitted_instruction[4].split(",")[0])+1):
+                for j in range(int(splitted_instruction[2].split(",")[1]), int(splitted_instruction[4].split(",")[1])+1):
+                    if splitted_instruction[1] == "off":
+                        roof[i][j] = 0
+                    else:
+                        roof[i][j] = 1
+        else:
+            for i in range(int(splitted_instruction[1].split(",")[0]), int(splitted_instruction[3].split(",")[0])+1):
+                for j in range(int(splitted_instruction[1].split(",")[1]), int(splitted_instruction[3].split(",")[1])+1):
+                    roof[i][j] = int(math.fabs(roof[i, j] - 1))
+    print(roof)
+    print(roof.sum())
