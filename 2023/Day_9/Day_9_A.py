18420baac73f32dc3a3bede8219c32aa66b66f01
Futym <mfutyma2000@gmail.com>
2023-12-14 21:17:53 +0100

    Day 9 Year 2023

---
+++ 2023/Day_9/Day_9_A.py
@@ -0,0 +1,22 @@
+def calculate_prev_elem(sequence):
+    if all(map(lambda x: x == 0, sequence)):
+        return 0
+    else:
+        new_sequence = []
+        for i in range(len(sequence)-1):
+            new_sequence.append(sequence[i+1] - sequence[i])
+        return sequence[-1] + calculate_prev_elem(new_sequence)
+
+
+def calculate_result(filename):
+    with open(filename, "r") as file:
+        res = 0
+        for line in file:
+            sequence = [int(x) for x in line.strip().split()]
+            res += calculate_prev_elem(sequence)
+        return res
+
+
+if __name__ == "__main__":
+    result = calculate_result("input.txt")
+    print(result)
